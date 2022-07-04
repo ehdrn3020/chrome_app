6405e0c192be6986d23c70583b8cb34bc68a9237
ehdrn3020 <ehdrnsla3020@naver.com>
2022-07-05 07:57:19 +0900

    20220705 init

---
+++ app.js
@@ -0,0 +1,11 @@
+const toBuy = ["pp","aaa"];
+toBuy.push("ccc");
+console.log(toBuy);
+
+const player = {
+    name: "dgk",
+    age: "30",
+    fat: false
+};
+player.weight = 77;
+console.log(player);
