28bc086e680a8f944b4d961989df9b60d1235af5
ehdrn3020 <ehdrnsla3020@naver.com>
2022-08-16 22:51:35 +0900

    20220816 js with css

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
 const title = document.querySelector('#title');
 
 function htClick() {
-    title.style.color = 'red';
-}
+    const clickedClass = 'active'
+    if(title.className === clickedClass){
+        title.className = "";
+    }
+    else {
+        title.className = clickedClass;
+    }
+} 
 
-function htMouseEnter() {
-    title.innerHTML = "HI HI";
-}
-
-function htMouseLeave() {
-    title.innerHTML = "BYE BYE";
-}
-
-function htWindowResize() {
-    document.body.style.backgroundColor = "skyblue";
-}
-console.dir(title);
-console.dir(document.body);
-
-title.onclick = htClick ;
-title.addEventListener("mouseenter", htMouseEnter);
-title.addEventListener("mouseleave", htMouseLeave);
-window.addEventListener("resize", htWindowResize);
+title.addEventListener("click", htClick);
