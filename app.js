5a5f4b77229ea5deb3ce6dcc6cdfb7177fb3c1b2
ehdrn3020 <ehdrnsla3020@naver.com>
2022-08-17 07:26:42 +0900

    20220817 classList with css

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const title = document.querySelector('#title');
+const title = document.querySelector('h1:first-child');
 
 function htClick() {
     const clickedClass = 'active'
-    if(title.className === clickedClass){
-        title.className = "";
-    }
-    else {
-        title.className = clickedClass;
-    }
+    // if(title.classList.contains(clickedClass)){
+    //     title.classList.remove(clickedClass);
+    // }
+    // else {
+    //     title.classList.add(clickedClass);
+    // }
+
+    // 위코드와 똑같은 기능
+    title.classList.toggle(clickedClass);
 } 
 
 title.addEventListener("click", htClick);
