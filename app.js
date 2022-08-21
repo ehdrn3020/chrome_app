4ea7079609520806f02644b4c46e7876ded35ca9
ehdrn3020 <ehdrnsla3020@naver.com>
2022-08-21 17:24:20 +0900

    20220821 get username and hidden form

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
 function onLogSubmit(event) {
     // submit event시 새로고침되는 기본 기능을 막기
     event.preventDefault();
     const username = loginInput.value;
-    console.log(username);
-}
+    loginForm.classList.add("hidden");
+    
+    greeting.innerText=`Hellow ${username}`;
+    greeting.classList.remove("hidden");
 
+}
 loginForm.addEventListener("submit", onLogSubmit);
