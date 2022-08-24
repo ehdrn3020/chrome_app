645585b48802ea9723863eee7e2e747991e4ed03
ehdrn3020 <ehdrnsla3020@naver.com>
2022-08-25 07:08:07 +0900

    20220825 create todo>ul>li and delete button

---
+++ js/todo.js
@@ -0,0 +1,31 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.getElementById("todo-list");
+
+function deleteToDo(event) {
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+function paintToDo(newToDo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    
+    span.innerText = newToDo;
+    button.innerText = "X";
+    button.addEventListener("click", deleteToDo);
+
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+    paintToDo(newToDo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
