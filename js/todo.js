23bfae63bcbd021c29588bc9ead482100ca557f4
ehdrn3020 <ehdrnsla3020@naver.com>
2022-08-26 08:33:04 +0900

    20220826 JSON.parse & array.forEach

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,13 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem("TODOS_KEY", JSON.stringify(TODOS_KEY));
+}
+
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
@@ -25,7 +32,19 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newToDo);
     paintToDo(newToDo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+if(savedToDos) {
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach(element => {
+        console.log('element is:', element);
+    });
+}
